67b3a7da8d7549c9825d2e94132e76f87c7830ff
JackWilb97 <jackwilburn@tutanota.com>
2018-11-08 21:49:29 -0700

    add login page and update navigation

--- src/MemeGenerator/views.py
+++ src/MemeGenerator/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
 	return render(request, 'MemeGenerator/index.html')
+
+def login(request):
+	return render(request, 'MemeGenerator/login.html')
