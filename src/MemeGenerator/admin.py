1f99bb19cba5ba68d82a657d3784cb54f4e2507e
JackWilb97 <jackwilburn@tutanota.com>
2018-11-25 16:33:29 -0700

    update admin.py

--- src/MemeGenerator/admin.py
+++ src/MemeGenerator/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Comment)
+admin.site.register(Likereaction)
+admin.site.register(Meme)
+admin.site.register(Memetag)
+admin.site.register(Tag)
+admin.site.register(Taguser)
+admin.site.register(Template)
+admin.site.register(Text)
+admin.site.register(User)
+admin.site.register(Usertemplate)
