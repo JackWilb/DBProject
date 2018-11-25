8fa95cfa0441858f632ec4267319a3f86c25dc70
JackWilb97 <jackwilburn@tutanota.com>
2018-11-25 16:35:32 -0700

    fix imports

--- src/MemeGenerator/admin.py
+++ src/MemeGenerator/admin.py
@@ -1,13 +1,5 @@
 from django.contrib import admin
+from MemeGenerator.models import Meme
 
 # Register your models here.
-admin.site.register(Comment)
-admin.site.register(Likereaction)
 admin.site.register(Meme)
-admin.site.register(Memetag)
-admin.site.register(Tag)
-admin.site.register(Taguser)
-admin.site.register(Template)
-admin.site.register(Text)
-admin.site.register(User)
-admin.site.register(Usertemplate)
