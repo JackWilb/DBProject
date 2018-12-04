63e45fe610cecbe7a9dab5e650474b0d1d87449a
JackWilb97 <jackwilburn@tutanota.com>
2018-12-04 11:45:02 -0700

    make templateid a template object (since its a foreign key)

--- src/MemeGenerator/views.py
+++ src/MemeGenerator/views.py
@@ -38,7 +38,12 @@ def makeameme(request):
 		else:
 			meme_id = 2
 
-		new_row_meme = Meme(templateid = meme_id, textid = Text.objects.get(top = topText).id, image = file_path, userid = None)
+		new_row_meme = Meme(
+			templateid = Template.objects.get(id=meme_id), 
+			textid = Text.objects.get(top = topText).id, 
+			image = file_path, 
+			userid = None
+		)
 		# Redirect off the page so we know it worked
 		return redirect('/static/MemeGenerator/styles.css')
 	else:
