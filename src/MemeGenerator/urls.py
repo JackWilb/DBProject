744c9175d85c7d212f888b04b8ba30105c3e17e9
JackWilb97 <jackwilburn@tutanota.com>
2018-12-06 23:34:33 -0700

    serve dynamic files attempt

--- src/MemeGenerator/urls.py
+++ src/MemeGenerator/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('makeameme/', views.makeameme, name='makeameme')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
