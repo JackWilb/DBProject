08c03b2e59dbfab911c716bfdfda87dd421f4df9
JackWilb97 <jackwilburn@tutanota.com>
2018-12-10 22:26:12 -0700

    add comment authors

--- src/MemeGenerator/views.py
+++ src/MemeGenerator/views.py
@@ -18,8 +18,8 @@ def index(request):
 			return redirect('/')
 
 		# Get 2 random indicies
-		randomindex1 = random.randint(0, Meme.objects.values('memetag', 'image').filter(memetag__tagid__in=tags).count() - 1)
-		randomindex2 = random.randint(0, Meme.objects.values('memetag', 'image').filter(memetag__tagid__in=tags).count() - 1)
+		randomindex1 = random.randint(0, Meme.objects.filter(memetag__tagid__in=tags).count() - 1)
+		randomindex2 = random.randint(0, Meme.objects.filter(memetag__tagid__in=tags).count() - 1)
 
 		if (randomindex1 == randomindex2):
 			randomindex2 = random.randint(0, Meme.objects.values('memetag', 'image').filter(memetag__tagid__in=tags).count() - 1)
