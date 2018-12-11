60f067bae44af2c640857f53d7eea8f7c77d13e3
JackWilb97 <jackwilburn@tutanota.com>
2018-12-10 20:32:31 -0700

    add comments to index

--- src/MemeGenerator/models.py
+++ src/MemeGenerator/models.py
@@ -10,7 +10,7 @@ from django.db import models
 
 
 class Comment(models.Model):
-    userid = models.ForeignKey('User', models.DO_NOTHING)
+    userid = models.ForeignKey('User', models.DO_NOTHING, db_column='userid')
     memeid = models.ForeignKey('Meme', models.DO_NOTHING, db_column='memeid')
     comment = models.CharField(max_length=512)
 
