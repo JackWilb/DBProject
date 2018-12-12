8ccfe448f66eaec33f6a37e18b48268b099c78e2
JackWilb97 <jackwilburn@tutanota.com>
2018-12-11 17:31:12 -0700

    add new meme templates and for loop for templates in makeameme

--- buildDB.sh
+++ buildDB.sh
@@ -1,12 +1,15 @@
+# Remove all stale memes
 rm src/media/MemeGenerator/*.jpg
 
+# Re-load the mysql data in
 mysql -u root -p <DBProject.sql
 
+# Build memes
 width=`identify -format %w ~/websites/DBProject/src/static/MemeGenerator/sociallyAwkwardPenguin.jpg`;   convert -background '#0008' -fill white -font Impact -gravity center -size ${width}x30 caption:"Hey, what's up?"  ~/websites/DBProject/src/static/MemeGenerator/sociallyAwkwardPenguin.jpg +swap -gravity north -composite ~/websites/DBProject/src/media/MemeGenerator/temp.jpg
 width=`identify -format %w ~/websites/DBProject/src/static/MemeGenerator/sociallyAwkwardPenguin.jpg`;   convert -background '#0008' -fill white -font Impact -gravity center -size ${width}x30 caption:"Good, you?"  ~/websites/DBProject/src/media/MemeGenerator/temp.jpg +swap -gravity south -composite ~/websites/DBProject/src/media/MemeGenerator/admin1.jpg
 
-
+# Build memes
 width=`identify -format %w ~/websites/DBProject/src/static/MemeGenerator/rickAstley.jpg`;   convert -background '#0008' -fill white -font Impact -gravity center -size ${width}x30 caption:"Never Gonna Give You Up"  ~/websites/DBProject/src/static/MemeGenerator/rickAstley.jpg +swap -gravity north -composite ~/websites/DBProject/src/media/MemeGenerator/admin2.jpg
 
-
+# Output setup complete when images are made
 echo "Setup Complete"
